2f28b93f9a1d7aac30db32a3497873b088eb041e
wuguangwei <1665382553@qq.com>
2020-07-23 19:06:38 +0800

    爬虫

--- 程序化/python-hello/taintain.py
+++ 程序化/python-hello/taintain.py
@@ -6,52 +6,81 @@ import pandas as pd
 import time
 import tiandetail as td
 
-response = requests.get(
-    # "http://fund.eastmoney.com/data/FundGuideapi.aspx?dt=0&sd=&ed=&sc=3y&st=desc&pi=1&pn=20&zf=diy&sh=list"
-    "http://fund.eastmoney.com/data/FundGuideapi.aspx?dt=0&rs=6y,100&sd=&ed=&sc=6y&st=desc&pi=1&pn=20&zf=diy&sh=list&rnd=0.8516450784808944"
-)
-if response.encoding.upper() != "UTF-8":
-    response.encoding = "UTF-8"
-html = response.text
+isFillColumn = False
 
-print(html)
 
-# 获取列表数据
-pattern = re.compile("(\{.*?\})")
-listDataStr = re.findall(pattern, html)
-jsonData = json.loads(listDataStr[0])
-datas = [jsonData['datas']][0]
+# 尝试爬取数据
+def craw():
+    pageNum = 1
+    url = 'http://fund.eastmoney.com/data/FundGuideapi.aspx?dt=0&sd=2016-07-23&ed=2020-07-23&pr=120.00,500.00&sc=diy&st=desc&pi=1&pn=20&zf=diy&sh=list&rnd=0.3138604478066571'
 
-splitData = []
-for data in datas:
-    tmp = data.split(',')
-    splitData.append(tmp)
+    # csv表头
+    csvColumn = []
+    # csv数据集合
+    csvData = []
+    while (True):
+        regex = re.compile("pi=\d")
+        url = regex.sub('pi=' + str(pageNum), url)
+        # url = url.replace('pi\d', 'pi' + str(pageNum))
+        response = requests.get(url)
+        if response.encoding.upper() != "UTF-8":
+            response.encoding = "UTF-8"
+        html = response.text
 
-columnFlied = ['代码', '基金名称', '基金类型', '净值', '日增长率', '近1周', '近1月', '近3月', '近6月', '今年来', '近1年', '近2年', '近3年', '手续费',
-               '购买起点']
-csvData = []
-for row in splitData:
-    id = row[0]
-    rowData = []
-    rowData.append(row[0])
-    rowData.append(row[1])
-    rowData.append(row[3])
-    rowData.append(row[16])
-    rowData.append(row[17] + '%')
-    rowData.append(row[5])
-    rowData.append(row[6])
-    rowData.append(row[7])
-    rowData.append(row[8])
-    rowData.append(row[4])
-    rowData.append(row[9])
-    rowData.append(row[10])
-    rowData.append(row[11])
-    rowData.append(row[19] + '|' + row[22])
-    rowData.append(row[20])
-    csvData.append(rowData)
-    td.detail(columnFlied, rowData, id)
+        print(html)
 
-csv = pd.DataFrame(columns=columnFlied, data=csvData)
-time = time.time()
-csv.to_csv('G:/garbage/csv/' + str(time) + '.csv')
-print(csvData)
+        # 获取列表数据
+        pattern = re.compile("(\{.*?\})")
+        listDataStr = re.findall(pattern, html)
+        jsonData = json.loads(listDataStr[0])
+        datas = [jsonData['datas']][0]
+        # 跳出爬虫
+        if len(datas) == 0:
+            break
+        # 解析数据并填充其他部分
+        parseAndFill(datas, csvColumn, csvData)
+        pageNum += 1
+
+    # 保存到csv
+    csv = pd.DataFrame(columns=csvColumn, data=csvData)
+    timestamp = time.time()
+    csv.to_csv('D:/garbage/csv/' + str(timestamp) + '.csv')
+
+
+def parseAndFill(datas, csvColumn, csvData):
+    splitData = []
+    for data in datas:
+        tmp = data.split(',')
+        splitData.append(tmp)
+    global isFillColumn
+    if isFillColumn == False:
+        csvColumn = ['代码', '基金名称', '基金类型', '净值', '日增长率', '近1周', '近1月', '近3月', '近6月', '今年来', '近1年', '近2年', '近3年', '手续费',
+                     '购买起点']
+        isFillColumn = True
+
+    for row in splitData:
+        id = row[0]
+        rowData = []
+        rowData.append(row[0])
+        rowData.append(row[1])
+        rowData.append(row[3])
+        rowData.append(row[16])
+        rowData.append(row[17])
+        rowData.append(row[5])
+        rowData.append(row[6])
+        rowData.append(row[7])
+        rowData.append(row[8])
+        rowData.append(row[4])
+        rowData.append(row[9])
+        rowData.append(row[10])
+        rowData.append(row[11])
+        rowData.append(row[19] + '|' + row[22])
+        rowData.append(row[20])
+        csvData.append(rowData)
+        try:
+            td.detail(csvColumn, rowData, id)
+        except:
+            print('处理的id=' + id + '的基金出错')
+
+# 开始爬虫
+craw()
--- 程序化/python-hello/tiandetail.py
+++ 程序化/python-hello/tiandetail.py
@@ -15,28 +15,38 @@ def detail(columns, rowData, id):
 
 # # 获取 成立日期、规模等信息
 def detail1(columns, rowData, id):
-    print(rowData[1])
+    # print(rowData[1])
     url = 'http://fund.eastmoney.com/' + id + '.html'
     resp = requests.get(url)
     resp.encoding = 'UTF-8'
     html = resp.text
     tree = etree.HTML(html)
-    tableData = tree.xpath('//*[@id="body"]/div[12]/div/div/div[3]/div[1]/div[2]/table//text()')
+    # tableData = tree.xpath('//div[@class="infoOfFund"]/table/tbody//text()')
+    tds = tree.xpath('//div[@class="infoOfFund"]//td')
+
+    tableData = []
+    for td in tds:
+        tmp = etree.tostring(td, encoding='utf-8', method="text").decode('utf-8')
+        tableData.append(tmp)
+
+    print(tableData)
 
     global fillColumns1
     if fillColumns1 == False:
         columns.append('基金规模(亿元)')
         columns.append('成 立 日')
         columns.append('管 理 人')
-        #columns.append('基金评级')
+        # columns.append('基金评级')
         fillColumns1 = True
-    tmp = str(tableData[4])
+    tmp = tableData[1]
     index = tmp.find('亿') - 1
-    size = tmp[1:index]
+    size = tmp[tmp.find("：")+1:index]
     rowData.append(size)
-    rowData.append(str(tableData[8])[1:-1])
-    rowData.append(tableData[11])
-    #rowData.append(tableData[14])
+    tmp = tableData[3]
+    rowData.append(tmp[tmp.find("：")+1:-1])
+    tmp = tableData[4]
+    rowData.append(tmp[tmp.find("：")+1:-1])
+    # rowData.append(tableData[14])
 
     print(columns)
     print(rowData)
@@ -64,17 +74,22 @@ def detail2(columns, rowData, id):
 
     manager = dataList[0]
 
-    rowData.append(manager['name'])
-    rowData.append(manager['workTime'])
-    p1 = manager['profit']['series'][0]['data'][0]['y']
-    p2 = manager['profit']['series'][0]['data'][1]['y']
-    p3 = manager['profit']['series'][0]['data'][2]['y']
-    rowData.append(p1)
-    rowData.append(p2)
-    rowData.append(p3)
-    rowData.append(p1 / p2)
-    rowData.append(p1 / p3)
+    try:
+        rowData.append(manager['name'])
+        rowData.append(manager['workTime'])
+        p1 = manager['profit']['series'][0]['data'][0]['y']
+        p2 = manager['profit']['series'][0]['data'][1]['y']
+        p3 = manager['profit']['series'][0]['data'][2]['y']
+        rowData.append(p1)
+        rowData.append(p2)
+        rowData.append(p3)
+        rowData.append(p1 / p2)
+        rowData.append(p1 / p3)
+    except:
+        print('处理的id='+id+'的基金出错')
 
     print(columns)
     print(rowData)
 
+
+##detail([], [], '519760')
